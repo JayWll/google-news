c3ec3e7bdca2975a4f3b88eb5b6f6b6f1680f1bd
Jason Williams <jaywll@outlook.com>
2019-10-12 11:54:50 -0600

    Finalized Google News proof of concept

--- utils/googlenews.js
+++ utils/googlenews.js
@@ -1,21 +1,17 @@
-const request = require('request')
-
-// const forecast = (latitude, longitude, callback) => {
-//   const url = 'https://api.darksky.net/forecast/0466bccb953c0b9daeb091a529da2c0d/' + latitude + ',' + longitude + '?units=si'
-//
-//   request({ url, json: true }, (error, {body}) => {
-//     if (error) {
-//       callback('Unable to connect to weather service!', undefined)
-//     } else if  (body.error) {
-//       callback('Unable to find location', undefined)
-//     } else {
-//       callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
-//     }
-//   })
-// }
+const feed = require('rss-to-json')
 
 const googlenews = (query) => {
-  console.log(query)
+  //console.log(query)
+
+  const url = 'https://news.google.com/rss/search?q=' + encodeURIComponent(query)
+
+  feed.load(url, (err, rss) => {
+    if (err) {
+      console.log('Unable to fetch news articles')
+    } else {
+      console.log(rss)
+    }
+  })
 }
 
 module.exports = googlenews
