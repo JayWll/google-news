8e4967da7397e24508c934f9861b30e36bb37a62
Jason Williams <jaywll@outlook.com>
2019-10-12 12:06:29 -0600

    Created /news route in express.

--- app.js
+++
@@ -1,4 +0,0 @@
-const gnews = require('./utils/googlenews.js')
-
-console.log('Hello, world!')
-gnews('Node.js development')
--- server.js
+++ server.js
@@ -2,18 +2,35 @@
 // where your node app starts
 
 // init project
-const express = require("express");
+const express = require('express');
+const feed = require('rss-to-json')
 const app = express();
 
 // we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function(request, response) {
-  response.sendFile(__dirname + "/views/index.html");
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/news', (req, res) => {
+  if (!req.query.q) {
+    return res.send({error: 'You must provide a search term.'})
+  }
+
+  const url = 'https://news.google.com/rss/search?q=' + req.query.q
+
+  feed.load(url, (err, rss) => {
+    if (err) {
+      return res.send({error: 'Unable to fetch news articles.'})
+    }
+
+    res.send(rss)
+  })
 });
 
 // listen for requests :)
